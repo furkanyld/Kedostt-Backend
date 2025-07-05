0842d35f10a31f08888d486ae76064948c0eba96
furkanyld <furkan16_yildiz@hotmail.com>
2025-07-05 23:27:28 +0300

    SecurityConfig updated.

--- src/main/java/com/kedostt_backend/Kedostt_Backend/security/SecurityConfig.java
+++ src/main/java/com/kedostt_backend/Kedostt_Backend/security/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
                 ))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/ping").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/animals/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/adoption/**").permitAll()
